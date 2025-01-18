fb71cfac7d52b9c10d44f082f860955893380111
PeaceIsuaiko <peaceisuaiko2000@gmail.com>
2025-01-18 02:19:18 +0100

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -8,7 +8,7 @@ function checkAnswer() {
 
 if (userAnswer) {
 
-    if (userAnswer === correctAnswer) {
+    if (userAnswer.value === correctAnswer) {
     feedback.textContent = 'correct! well done';
     feedback.style.color = 'green';
     } else {
